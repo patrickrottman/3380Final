aa7c816d08c8a4c96b2d4fbe0de9728f63c731d5
Patrick Rottman <pjrc74@missouri.edu>
2017-12-05 22:41:58 -0600

    Added index.php and header.php
    Started initial page layout. next step will be the landing page php

---
+++ header.php
@@ -0,0 +1,26 @@
+<style>
+    .navbar {
+
+        padding-bottom: 0px;
+    }
+
+</style>
+
+<nav class="navbar">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="/">Permanency Team</a>
+        </div>
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Foster Children</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Add Child</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Your Account</a>
+            </li>
+        </ul>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Permanency Team</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <style>
+        html {
+            background-color: lightgray;
+        }
+
+    </style>
+
+    <script>
+
+
+    </script>
+</head>
+
+<body>
+    <?php include 'header.php';?>
+    <div class="offset-1 col-10 offset-1" id="center">
+        
+    </div>
+
+
+
+</body>
+
+</html>
